6659cd47e74468b2747096a766f02f6a9d3b7817
Chad Stolper <chadstolper@gmail.com>
2013-11-22 15:06:59 -0500

    X,Y Transitions
    First set of transitions, namely removing links, moving x position, and
    moving y position

--- js/les_mis_demo.js
+++ js/les_mis_demo.js
@@ -1,17 +1,34 @@
-var width = 900
+/*
+  MLGO Force-Directed ---> Scatterplot Steps
+  0) Force-Directed
+  1) Transition x position based on betweenness_centrality
+  2) Add x axis
+  3) Transition y position based on degree
+  4) Add y axis
+*/
+
+var graph = {}
+var node, link
+var xscale, yscale
+var transition_duration = 2000
+
+var mlgo = d3.select("#mlgo")
+
+
+
+var width = 700
 var height = 500
-
 var color = d3.scale.category20()
-
-var force = d3.layout.force()
-    .charge(-200)
-    .linkDistance(75)
-    .size([width,height])
 
 var svg = d3.select("#canvas").append("svg")
     .attr("width",width)
     .attr("height",height)
 
+
+
+
+
+//Load Data
 d3.csv("data/LesMis/nodes.csv",function(nodes){
   d3.csv("data/LesMis/edges.csv",function(edges){
     nodes.forEach(function(d){
@@ -38,25 +55,39 @@ d3.csv("data/LesMis/nodes.csv",function(nodes){
       d.weight = +d.weight
     })
 
+    graph.nodes = nodes
+    graph.edges = edges
 
-    force
-      .nodes(nodes)
-      .links(edges)
+  })
+})
+
+
+var force_directed = function(){
+  mlgo.style("visibility","hidden")
+
+  var force = d3.layout.force()
+    .charge(-200)
+    .linkDistance(75)
+    .size([width,height])
+
+  force
+      .nodes(graph.nodes)
+      .links(graph.edges)
       .start()
 
-    var link = svg.selectAll(".link")
-        .data(edges)
+    link = svg.selectAll(".link")
+        .data(graph.edges)
       .enter().append("line")
         .classed("link",true)
         .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
 
-    var node = svg.selectAll(".node")
-        .data(nodes)
+    node = svg.selectAll(".node")
+        .data(graph.nodes)
       .enter().append("circle")
         .classed("node",true)
         .attr("r", function(d){ return d.degree; })
         .attr("fill", function(d){ return color(d.modularity_class); })
-        .call(force.drag)
+        //.call(force.drag)
 
     node.append("title")
       .text(function(d){ return d.label; })
@@ -70,5 +101,80 @@ d3.csv("data/LesMis/nodes.csv",function(nodes){
       node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
     })
-  })
+
+    force.on("end",function(){
+      mlgo.style("visibility","visible")
+    })
+}
+
+var hide_links = function(){
+  link.style("visibility","hidden")
+}
+
+
+var transition_x = function(){
+  xscale = d3.scale.linear()
+    .range([0,width])
+    .domain([0,d3.max(graph.nodes.map(function(d){return d.betweenness_centrality; }))])
+    .nice()
+
+
+  node.transition().duration(transition_duration)
+    .attr("cx",function(d){
+      d.x = xscale(d.betweenness_centrality)
+      return d.x
+    })
+
+  link.transition().duration(transition_duration)
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("x2", function(d) { return d.target.x; })
+
+}
+
+
+var transition_y = function(){
+  yscale = d3.scale.linear()
+    .range([height,0])
+    .domain([0,d3.max(graph.nodes.map(function(d){return d.degree; }))])
+    .nice()
+
+  node.transition().duration(transition_duration)
+    .attr("cy",function(d){
+      d.y = yscale(d.degree)
+      return d.y
+    })
+
+  link.transition().duration(transition_duration)
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("y2", function(d) { return d.target.y; });
+}
+
+// force_directed = function(){}
+// var hide_links = function(){}
+// var transition_x = function(){}
+var draw_x_axis = function(){}
+// var transition_y = function(){}
+var draw_y_axis = function(){}
+var finished = function(){
+  mlgo.attr("disabled","true")
+}
+
+
+//The Button
+var mlgo_index = 0
+var mlgo_steps = [
+  {f:force_directed, label:"Force-Directed"},
+  {f:hide_links, label:"Hide Links"},
+  {f:transition_x, label:"Transition X"},
+  {f:draw_x_axis, label:"Draw X Axis"},
+  {f:transition_y, label:"Transition Y"},
+  {f:draw_y_axis, label:"Draw Y Axis"},
+  {f:finished, label:"Finished"}
+  ]
+mlgo.text(mlgo_steps[mlgo_index].label)
+mlgo.on("click",function(){
+  mlgo_steps[mlgo_index++].f()
+  if(mlgo_index<mlgo_steps.length){
+    mlgo.text(mlgo_steps[mlgo_index].label)
+  }
 })
