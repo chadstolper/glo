bb1245c34bc8f29f9316cd62e04c3ff195d52d91
Minsuk Kahng <bmkahng@gmail.com>
2015-02-26 03:05:57 -0500

    database connection

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ import web
 import simplejson as json
 import ast
 import csv
+import MySQLdb
+from collections import defaultdict
 
 urls = (
 	'/', 'gui',
-	'/data/(.*)', 'data_request',
+	'/data/(\d+)', 'data_request',
 	'/(.*)', 'dataset',
 )
 
@@ -25,18 +27,48 @@ class dataset:
 
 class data_request:
 	def GET(self, id):
+		id = int(id)
 		d = {}
-		nodes = []
-		edges = []
-		nodes.append( ["id", "label", "modularity_class"] )
-		edges.append( ["source", "target", "type", "id", "label", "weight"] )
+
+		database = ""
+		q = ""
+		if id == 1:
+			database = "prodigalobservation"
+			q = "SELECT source, target, weight  FROM   (  SELECT fromUser AS source, touser AS target, COUNT(*) AS weight  FROM imLink  WHERE eventdate>='2014-05-05' AND eventdate<'2014-05-19'   AND FLOOR(fromuser/1000)%10=1 AND FLOOR(touser/1000)%10=1  GROUP BY fromuser, touser  ) e   NATURAL JOIN  (	SELECT fromuser AS source  	FROM imLink   	WHERE eventdate>='2014-05-05' AND eventdate<'2014-05-19'   	AND FLOOR(fromuser/1000)%10=1 AND FLOOR(touser/1000)%10=1  	GROUP BY fromuser  	HAVING COUNT(DISTINCT touser) >= 3  ) u  "
+		elif id == 2:
+			database = "prodigalresult"
+			q = "select fromImID, toImID, weight FROM imGraph WHERE (toImID = 9000 or fromImID = 9000) and eventDate between '2014-05-01' and '2014-05-31';"
+		
+
+		db = MySQLdb.connect(host="prodigal5", user="prodigal", passwd="prodigal", port=3307, db=database)
+		cur = db.cursor() 
+		cur.execute(q)
+
+		edges = cur.fetchall()
+		
+		degree = defaultdict(int)
+		for e in edges:
+			degree[e[0]] += 1
+			degree[e[1]] += 1
+		
+		nodes_set = set()
+		for e in edges:
+			nodes_set.add(e[0])
+			nodes_set.add(e[1])
+		vno = {}
+		no = 0
+		for v in nodes_set:
+			vno[v] = no
+			no += 1
 		
 		with open("./static/data/dynamic/nodes.csv", "wb") as f:
 			writer = csv.writer(f, delimiter=',')
-			writer.writerows(nodes)
+			writer.writerow( ["id", "label", "modularity_class", "degree"] )
+			writer.writerows([[vno[v], v, 0, degree[v]] for v in nodes_set])
 		with open("./static/data/dynamic/edges.csv", "wb") as f:
 			writer = csv.writer(f, delimiter=',')
-			writer.writerows(edges)
+			writer.writerow( ["source", "target", "type", "id", "label", "weight"] )
+			writer.writerows([[vno[edges[i][0]], vno[edges[i][1]], "Directed", i, "", edges[i][2]] for i in range(len(edges))])
 		
 		d["dataset"] = "dynamic"
 		return render.gui(d)
