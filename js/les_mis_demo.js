66659154027f235fa6530c53f9af1163c4eb531c
vdidonato <valentino.didonato@gmail.com>
2016-02-10 18:55:02 -0500

    missing semicolons

--- js/les_mis_demo.js
+++ js/les_mis_demo.js
@@ -11,7 +11,7 @@
 
 
 // var source = "PivotGraph"
-var source = "LesMis"
+var source = "LesMis";
 // var source = "TinyToy"
 
 
@@ -19,41 +19,41 @@ var source = "LesMis"
 d3.csv("data/"+source+"/nodes.csv",function(nodes){
   d3.csv("data/"+source+"/edges.csv",function(edges){
     nodes.forEach(function(d){
-      d.id = +d.id
-      d.modularity_class = +d.modularity_class
-      d.degree = +d.degree
-      d.weighted_degree = +d.weighted_degree
-      d.eccentricity = +d.eccentricity
-      d.closeness_centrality = +d.closeness_centrality
-      d.betweenness_centrality = +d.betweenness_centrality
-      d.authority = +d.authority
-      d.hub = +d.hub
-      d.page_rank = +d.page_rank
-      d.component_id = +d.component_id
-      d.clustering_coefficient = +d.clustering_coefficient
-      d.number_of_triangles = +d.number_of_triangles
-      d.eigenvector_centrality = +d.eigenvector_centrality
-      d.x_list = {}
-      d.y_list = {}
-      d.r_list = {}
-      d.radius_list = {}
+      d.id = +d.id;
+      d.modularity_class = +d.modularity_class;
+      d.degree = +d.degree;
+      d.weighted_degree = +d.weighted_degree;
+      d.eccentricity = +d.eccentricity;
+      d.closeness_centrality = +d.closeness_centrality;
+      d.betweenness_centrality = +d.betweenness_centrality;
+      d.authority = +d.authority;
+      d.hub = +d.hub;
+      d.page_rank = +d.page_rank;
+      d.component_id = +d.component_id;
+      d.clustering_coefficient = +d.clustering_coefficient;
+      d.number_of_triangles = +d.number_of_triangles;
+      d.eigenvector_centrality = +d.eigenvector_centrality;
+      d.x_list = {};
+      d.y_list = {};
+      d.r_list = {};
+      d.radius_list = {};
       d.theta_list = {}
-    })
+    });
     edges.forEach(function(d){
       // console.log(d)
-      d.source = +d.source
-      d.target = +d.target
+      d.source = +d.source;
+      d.target = +d.target;
       // d.type = +d.type
-      d.id = +d.id
+      d.id = +d.id;
       d.weight = +d.weight
-    })
+    });
 
-    graph.nodes = nodes
-    graph.edges = edges
+    graph.nodes = nodes;
+    graph.edges = edges;
 
     initialize_force_directed()
   })
-})
+});
 
 
 
