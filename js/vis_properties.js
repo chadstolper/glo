2d76ea7d5a2aa025718aff6207b82cb8d66de906
vdidonato <valentino.didonato@gmail.com>
2016-02-11 10:03:40 -0500

    semicolons and some comments

--- js/vis_properties.js
+++ js/vis_properties.js
@@ -1,18 +1,18 @@
-var graph = {}
-var node, nodeclone, link
-var node_generations = {}
-var link_generations = {}
-var agg_generations = {}
-var subgraph_generations = {}
-var xscale, yscale
-var xprop, yprop
-var transition_duration = 500
+var graph = {};
+var node, nodeclone, link;
+var node_generations = {};
+var link_generations = {};
+var agg_generations = {};
+var subgraph_generations = {};
+var xscale, yscale;
+var xprop, yprop;
+var transition_duration = 500;
 
 
-var y_substrates
-var x_substrates
+var y_substrates;
+var x_substrates;
 
-var force
+var force;
 
 
 var modes = {
@@ -24,23 +24,25 @@ var modes = {
   target_generation: 0,
   link_generation: 0,
   active_link_generation: 0,
-  generation: 0,
-  link_generation: 0,
-}
+  generation: 0
+};
 
 var node_data = function(){
   return node_generations[modes.active_generation].data()
-}
+};
+
 var link_data = function(){
   return link_generations[modes.active_link_generation].data()
-}
+};
 
+//TODO: Isn't this the same as return agg_generations[modes.active_generation]?
+//TODO: Or you are using "if" for something that does not return a boolean value? Valentino.
 var activeGenIsAggregate = function(){
   if(agg_generations[modes.active_generation]){
     return true
   }
   return false
-}
+};
 
 var num_gen = function(){
   var i = 0;
@@ -50,11 +52,11 @@ var num_gen = function(){
     }
   }
   return i;
-}
+};
 
 var generation_index = function(gen) {
-  var map = {}
-  var i = 0
+  var map = {};
+  var i = 0;
   for(var g in node_generations){
     if(node_generations[g]!=null && g!=gen){
       i++
@@ -62,35 +64,35 @@ var generation_index = function(gen) {
       return i/num_gen();
     }
   }
-}
+};
 
-var width = 610
-var height = 490
-var xbuffer = 50
-var ybuffer = 50
-var color = d3.scale.category10()
-var link_r = 2
-var max_link_curve_r = 15
-var node_r_constant = 8
+var width = 610;
+var height = 490;
+var xbuffer = 50;
+var ybuffer = 50;
+var color = d3.scale.category10();
+var link_r = 2;
+var max_link_curve_r = 15;
+var node_r_constant = 8;
 
-var xaxis = false
-var yaxis = false
+var xaxis = false;
+var yaxis = false;
 
 var svg = d3.select("#canvas").append("svg")
     .attr("width",width+(2*xbuffer))
-    .attr("height",height+(2*ybuffer))
+    .attr("height",height+(2*ybuffer));
 
 var chart = svg.append("g")
-  .attr("transform","translate("+xbuffer+","+ybuffer+")")
+  .attr("transform","translate("+xbuffer+","+ybuffer+")");
 
 var x_axis_g = svg.append("g")
     .attr("class","x axis")
-    .attr("transform","translate("+xbuffer+","+ (height+ybuffer+20) + ")")
+    .attr("transform","translate("+xbuffer+","+ (height+ybuffer+20) + ")");
 
 var y_axis_g = svg.append("g")
   .attr("class","y axis")
-  .attr("transform","translate("+(xbuffer-20)+","+ybuffer+")")
+  .attr("transform","translate("+(xbuffer-20)+","+ybuffer+")");
 
 
-var linkg = chart.append("g")
-var nodeg = chart.append("g")
+var linkg = chart.append("g");
+var nodeg = chart.append("g");
