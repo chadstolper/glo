fd635242aae226c28836a97dd1c3b60ca1a1e4d4
Chad Stolper <chadstolper@gmail.com>
2016-05-18 20:02:10 -0400

    More Techniques

--- oo/techniques.js
+++ oo/techniques.js
@@ -1,6 +1,7 @@
 
 GLO.GLO.prototype.Technique_Force_Directed = function(color_by_attr){
 	this.display_edges_as_straight_lines()
+	this.show_all_edges()
 	this.size_nodes_by_constant()
 	this.size_edges_by_constant()
 	this.color_edges_by_constant()
@@ -23,6 +24,7 @@ GLO.GLO.prototype.Technique_Matrix_Plot = function(sort_attr,edge_color_attr){
 	this.evenly_distribute_nodes_on("x",{by:sort_attr})
 	this.set_target_generation(1)
 	this.display_edges_as_squares()
+	this.show_all_edges()
 	this.size_edges_by_constant()
 	this.color_edges_by(edge_color_attr)
 
@@ -32,24 +34,61 @@ GLO.GLO.prototype.Technique_Matrix_Plot = function(sort_attr,edge_color_attr){
 
 
 GLO.GLO.prototype.Technique_Cluster_Circles = function(group_by_attr, internal_sort_attr){
-	this.size_nodes_by_constant()
+	this.show_all_edges()
 	this.display_edges_as_straight_lines()
+	this.color_edges_by_constant()
+	this.size_nodes_by_constant()
+	this.size_edges_by_constant()
 	this.evenly_distribute_nodes_on("theta",{by:group_by_attr})
 	this.position_nodes_by_constant_on("rho")
-
 	this.evenly_distribute_nodes_on("theta",{by:internal_sort_attr, group_by:group_by_attr})
 	this.position_nodes_by_constant_on("rho",{group_by:group_by_attr})
-
-
 
 	return this
 }
 
+
+
 GLO.GLO.prototype.Technique_Circle_Graph = function(sort_attr){
-	this.size_nodes_by_constant()
+	this.show_all_edges()
 	this.display_edges_as_straight_lines()
+	this.color_edges_by_constant()
+	this.size_edges_by_constant()
+	this.size_nodes_by_constant()
 	this.evenly_distribute_nodes_on("theta",{by:sort_attr})
 	this.position_nodes_by_constant_on("rho")
+
+	return this
+}
+
+
+GLO.GLO.prototype.Technique_GeneVis_A = function(position_attr){
+	this.hide_edges()
+	this.position_nodes_on("theta",position_attr)
+	this.position_nodes_by_constant_on("rho")
+	this.size_nodes_by_constant()
+
+	return this
+}
+
+GLO.GLO.prototype.Technique_GeneVis_B = function(discrete1,attr2){
+	this.show_all_edges()
+	this.display_edges_as_curved_lines()
+	this.size_edges_by_constant()
+	this.color_edges_by_constant()
+	this.position_nodes_on("y",discrete1)
+	this.position_nodes_on("x",attr2)
+
+	return this
+}
+
+GLO.GLO.prototype.Technique_Arc_Diagram = function(sort_attr){
+	this.display_edges_as_curved_lines()
+	this.size_edges_by_constant()
+	this.color_edges_by_constant()
+	this.show_all_edges()
+	this.align_nodes("middle")
+	this.evenly_distribute_nodes_on("x",{by:sort_attr})
 
 	return this
 }
